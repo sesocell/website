978f143e87698e0945b3de5e2c15f6395629132b
semih <146653658+sesocell@users.noreply.github.com>
2025-12-24 11:28:52 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from "@astrojs/svelte";
 export default defineConfig({
   integrations: [svelte({ extensions: [".svelte"] })],
   // To deploy to GitHub Pages, configure 'site' and 'base':
-  // site: 'https://YOUR_USERNAME.github.io',
-  // base: '/YOUR_REPO_NAME',
+   site: 'https://sesocell.github.io',
+   base: '/website',
 });
